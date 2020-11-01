8ecc20cf7a8f07b754af37e0a9a6d8702159e478
Salim Fadhley <salimfadhley@gmail.com>
2020-11-01 20:42:21 +0000

    The bot is now echoing.

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
-
 """
 First, a few handler functions are defined. Then, those functions are passed to
 the Dispatcher and registered at their respective places.
@@ -16,13 +15,10 @@ import os
 from uuid import uuid4
 
 from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import escape_markdown
 
-# Enable logging
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+log = logging.getLogger(__name__)
 
 logger = logging.getLogger(__name__)
 
@@ -41,30 +37,14 @@ def help_command(update, context):
     update.message.reply_text('Help!')
 
 
-def inlinequery(update, context):
-    """Handle the inline query."""
-    query = update.inline_query.query
-    results = [
-        InlineQueryResultArticle(
-            id=uuid4(), title="Caps", input_message_content=InputTextMessageContent(query.upper())
-        ),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="Bold",
-            input_message_content=InputTextMessageContent(
-                "*{}*".format(escape_markdown(query)), parse_mode=ParseMode.MARKDOWN
-            ),
-        ),
-        InlineQueryResultArticle(
-            id=uuid4(),
-            title="Italic",
-            input_message_content=InputTextMessageContent(
-                "_{}_".format(escape_markdown(query)), parse_mode=ParseMode.MARKDOWN
-            ),
-        ),
-    ]
+def foo_command(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Foo!')
 
-    update.inline_query.answer(results)
+
+def echo(update, context):
+    log.warning("Hello!")
+    update.message.reply_text(update.message.text)
 
 
 def main():
@@ -80,8 +60,10 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_command))
 
+    dp.add_handler(CommandHandler("foo", foo_command))
+
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(InlineQueryHandler(inlinequery))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     # Start the Bot
     updater.start_polling()
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,9 +1,12 @@
 import logging
 from bot.bot import main as bot_main
 
+log = logging.getLogger(__name__)
+
 def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     bot_main()
 
 if __name__ == "__main__":
+    log.info("Starting the bot!")
     main()
